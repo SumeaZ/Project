3815a879242c0ef4cd1b09d7cc2e0e0f032d2302
SumeaZ <sumeazogaj5@gmail.com>
2024-02-02 15:41:30 +0100

    Delete deleteClearance.php

--- deleteClearance.php
+++
@@ -1,9 +0,0 @@
-<?php
-$clearanceId = $_GET['id'];
-include_once 'clearanceR.php';
-
-$clearanceRepository = new ClearanceR();
-$clearanceRepository->deleteClearance($clearanceId);
-
-header("location:clearances.php");
-?>
